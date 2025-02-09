550c83568fa9cb07137b7ad9090edf58e695a8c3
Daphne Dao <daphnedao19@gmail.com>
2025-02-08 23:10:48 -0800

    Revised nav screen with button functionality

--- Prototype Development/Core/Src/display.c
+++ Prototype Development/Core/Src/display.c
@@ -2,6 +2,10 @@
 #include <stdbool.h>
 #include "display.h"
 #include "ssd1306.h"
+#include "ssd1306_fonts.h"
+
+extern int selectedButton;
+extern bool selectedPress;
 
 // Initialization function
 void SRE_Display_Init(bool test_mode) {
@@ -19,5 +23,107 @@ void SRE_Display_Test() {
 
 // Example function to display navigation
 void SRE_Display_Nav() {
+	selectedButton = 0;
 
+	char nav[] = "Navigation";
+	char home[] = "Home";
+	char charge[] = "Start Charging";
+	char balance[] = "Start Balancing";
+	char battery[] = "Battery";
+
+	char retval;
+
+	// 4 buttons: home, charging, balancing, battery
+	while(!selectPress) {
+		if (selectedButton > 3){
+			selectedButton = 0;
+		}
+		if (selectedButton < 0) {
+			selectedButton = 3;
+		}
+
+		//Navigation title
+		ssd1306_SetCursor(1, 1);
+		retval = ssd1306_WriteString(nav, Font_16x15, White);
+
+		ssd1306_Line(0, 10, 127, 10, White);
+
+		// Home
+		if (selectedButton == 0 || selectedButton > 4) {
+			ssd1306_SetCursor(3, 15);
+			retval = ssd1306_WriteString(home, Font_16x15, Black);
+			ssd1306_FillRectangle(1, 13, 122, 11, White);
+		}
+		else {
+			ssd1306_SetCursor(3, 15);
+			retval = ssd1306_WriteString(home, Font_16x15, White);
+			ssd1306_DrawRectangle(1, 13, 122, 11, White);
+		}
+
+		// Charging
+		if (selectedButton == 1) {
+			ssd1306_SetCursor(3, 28);
+			retval = ssd1306_WriteString(charge, Font_16x15, Black);
+			ssd1306_FillRectangle(1, 26, 122, 11, White);
+		}
+		else {
+			ssd1306_SetCursor(3, 28);
+			retval = ssd1306_WriteString(charge, Font_16x15, White);
+			ssd1306_DrawRectangle(1, 26, 122, 11, White);
+		}
+
+		// Balancing
+		if (selectedButton == 2) {
+			ssd1306_SetCursor(3, 41);
+			retval = ssd1306_WriteString(balance, Font_16x15, Black);
+			ssd1306_FillRectangle(1, 39, 122, 11, White);
+		}
+		else {
+			ssd1306_SetCursor(3, 41);
+			retval = ssd1306_WriteString(balance, Font_16x15, White);
+			ssd1306_DrawRectangle(1, 39, 122, 11, White);
+		}
+
+		// Battery
+		if (selectedButton == 3 || selectedButton < 0) {
+			ssd1306_SetCursor(3, 54);
+			retval = ssd1306_WriteString(battery, Font_16x15, Black);
+			ssd1306_FillRectangle(1, 52, 122, 11, White);
+		}
+		else {
+			ssd1306_SetCursor(3, 54);
+			retval = ssd1306_WriteString(battery, Font_16x15, White);
+			ssd1306_DrawRectangle(1, 52, 122, 11, White);
+		}
+
+		ssd1306_UpdateScreen();
+	}
+
+	if (selectPressed) {
+		selectPressed = false;
+
+		if (selectedButton > 3) {
+			selectedButton = 0;
+		}
+		if (selectedButton < 0) {
+			selectedButton = 1;
+		}
+
+		// Populate with the function name that corresponds to each button number respectively later.
+		if (selectButton == 0) {
+			// Goes to home
+		}
+		else if (selectButton == 1) {
+			// Goes to Charging
+
+		}
+		else if (selectButton == 2) {
+			// Goes to Balancing
+
+		}
+		else if (selectButton == 3) {
+			// Goes to Battery
+
+		}
+	}
 }
