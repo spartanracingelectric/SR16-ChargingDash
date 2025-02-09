0546663b2d59e2d3cf2c6836cc51e4684940599d
Daphne Dao <daphnedao19@gmail.com>
2025-02-09 00:59:01 -0800

    revised error screen to match code format

--- Prototype Development/Core/Src/display.c
+++ Prototype Development/Core/Src/display.c
@@ -2,6 +2,10 @@
 #include <stdbool.h>
 #include "display.h"
 #include "ssd1306.h"
+#include "ssd1306_fonts.h"
+
+extern int selectedButton;
+extern bool selectedPress;
 
 // Initialization function
 void SRE_Display_Init(bool test_mode) {
@@ -23,45 +27,64 @@ void SRE_Display_Nav() {
 }
 
 void SRE_Display_Err() {
-	// Error title
+	selectedButton = 0;
+
 	char err[] = "Errors";
-	char retval;
-
-	ssd1306_SetCursor(1, 1);
-	retval = ssd1306_WriteString(err, Font_16x15, White);
-
-	// Draw line
-	ssd1306_Line(0, 10, 127, 10, White);
-
-	// Sample error msg
 	char err1[] = "Error message 1";
-	char retval;
-
-	ssd1306_SetCursor(3, 15);
-	retval = ssd1306_WriteString(err1, Font_16x15, White);
-
-	// Draw Rect
-	ssd1306_DrawRectangle(1, 13, 122, 11, White);
-
-	// Sample error 2 msg
 	char err2[] = "Error message 2";
-	char retval;
-
-	ssd1306_SetCursor(3, 28);
-	retval = ssd1306_WriteString(err2, Font_16x15, White);
-
-	// Draw Rect
-	ssd1306_DrawRectangle(1, 26, 122, 11, White);
-
-	// Sample error 3 msg
 	char err3[] = "Error message 3";
+
 	char retval;
 
-	ssd1306_SetCursor(3, 41);
-	retval = ssd1306_WriteString(err3, Font_16x15, White);
+	while (!selectedPress) {
+		ssd1306_FillRectangle(0, 0, 127, 63, Black);
 
-	// Draw Rect
-	ssd1306_DrawRectangle(1, 39, 122, 11, White);
+		if (selectedButton > 1) {
+			selectedButton = 0;
+		}
+		if (selectedButton < 0) {
+			selectedButton = 1;
+		}
 
-	ssd1306_UpdateScreen();
+		ssd1306_SetCursor(1, 1);
+		retval = ssd1306_WriteString(err, Font_16x15, White);
+		ssd1306_Line(0, 10, 127, 10, White);
+
+		ssd1306_SetCursor(3, 15);
+		retval = ssd1306_WriteString(err1, Font_16x15, White);
+		ssd1306_DrawRectangle(1, 13, 122, 11, White);
+
+		ssd1306_SetCursor(3, 28);
+		retval = ssd1306_WriteString(err2, Font_16x15, White);
+		ssd1306_DrawRectangle(1, 26, 122, 11, White);
+
+		ssd1306_SetCursor(3, 41);
+		retval = ssd1306_WriteString(err3, Font_16x15, White);
+		ssd1306_DrawRectangle(1, 39, 122, 11, White);
+
+		char *navBarButtons[] = {"Batt", "Nav"};
+
+		SRE_Display_Nav_Bar(navBarButtons, 2, 0);
+
+		ssd1306_UpdateScreen();
+	}
+
+	if (selectedPress) {
+		selectedPress = false;
+
+		if (selectedButton > 1) {
+			selectedButton = 0;
+		}
+		if (selectedButton < 0) {
+			selectedButton = 1;
+		}
+
+		if (selectedButton == 0) {
+			// Goes to Batt
+		}
+		else if (selectedButton == 1) {
+			// Goes to Nav
+		}
+	}
+
 }
