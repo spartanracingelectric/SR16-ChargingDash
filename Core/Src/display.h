10281f0505e2fa9f5a8231bf0179c06ea8d5387a
Daphne Dao <daphnedao19@gmail.com>
2025-02-21 23:26:19 -0800

    Created error screen for refractored repo

--- Core/Src/display.h
+++ Core/Src/display.h
@@ -7,4 +7,5 @@ void SRE_Display_Init(bool test_mode);
 void SRE_Display_Test();
 void SRE_Display_Nav();
 void SRE_Display_Nav_Bar(char *buttons[], int numOfButtons, int firstButtonIndex);
+void SRE_Display_Err();
 #endif
