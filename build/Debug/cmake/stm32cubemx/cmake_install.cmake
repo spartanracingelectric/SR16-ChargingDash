de593cc4163985f888f3805525fa274ff62e4230
Bhuvanesh Selvaraj <bhuvanesh.sel@gmail.com>
2025-04-03 18:32:50 -0700

    STM32 VS Code test

--- build/Debug/cmake/stm32cubemx/cmake_install.cmake
+++ build/Debug/cmake/stm32cubemx/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Owner/Documents/GitHub/SR16-ChargingDash/cmake/stm32cubemx
+# Install script for directory: C:/Users/Bhuv/Desktop/SR16-ChargingDash/cmake/stm32cubemx
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
--- build/Debug/cmake_install.cmake
+++ build/Debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Owner/Documents/GitHub/SR16-ChargingDash
+# Install script for directory: C:/Users/Bhuv/Desktop/SR16-ChargingDash
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -39,7 +39,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/Owner/Documents/GitHub/SR16-ChargingDash/build/Debug/cmake/stm32cubemx/cmake_install.cmake")
+  include("C:/Users/Bhuv/Desktop/SR16-ChargingDash/build/Debug/cmake/stm32cubemx/cmake_install.cmake")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Owner/Documents/GitHub/SR16-ChargingDash/build/Debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Bhuv/Desktop/SR16-ChargingDash/build/Debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
