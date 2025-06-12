63c9aab7f554c696e01746aac8832410c465db44
Peltogle <42919137+peltogle@users.noreply.github.com>
2025-06-12 15:16:32 -0700

    Added live updates to therm

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -42,8 +42,8 @@
 #define FLAG_622 (1 << 2)
 
 // TODO: move stuff here
-#define UPPER_MAX_CELL_CV_THRESH 4.25 // Competition
-#define LOWER_MAX_CELL_CV_THRESH 4.1
+#define UPPER_MAX_CELL_CV_THRESH 4.7 // Competition is 4.25
+#define LOWER_MAX_CELL_CV_THRESH 4.6 // Competition is 4.1
 #define MIN_ALLOWED_IMBAL 0.01
 #define MAX_ALLOWED_PWR 4000
 #define MAINT_AMPS 0.5
@@ -169,12 +169,15 @@ void FAN_SPD_CTRL(uint32_t fan_speed) {
 // TODO: fix
 // READ THERMISTOR VALUE
 float READ_THERM(uint16_t* adc_thermistor, uint16_t therm_first_resistance) {
+  if (adc_thermistor == NULL || *adc_thermistor == 0) return -273.15;
   float calibration_a = 1.462805229e-3;
   float calibration_b = 2.310582766e-4;
   float calibration_c = 1.189005910e-7;
   float thermistor_voltage = (*adc_thermistor / 4095.0) * 3.3;
-  float therm_inlet_resistance = (thermistor_voltage * therm_first_resistance) / (3.3 - thermistor_voltage);
-  float temperature = (1 / (calibration_a + calibration_b * log(therm_inlet_resistance) + calibration_c * pow(log(therm_inlet_resistance), 3))) - 273.15;
+  if (thermistor_voltage >= 3.3f) thermistor_voltage = 3.299;
+  float therm_resistance = (thermistor_voltage * therm_first_resistance) / (3.3 - thermistor_voltage);
+  if (therm_resistance <= 0) return -273.15;
+  float temperature = (1 / (calibration_a + calibration_b * log(therm_resistance) + calibration_c * pow(log(therm_resistance), 3))) - 273.15;
   return temperature;
 }
 
@@ -184,8 +187,9 @@ void NEOPIX_CTRL(int SOC) {
 }
 
 // TODO: complete
-// READ CONTROL PILOT
-void READ_CPILOT() {
+// READ CONTROL PILOT FOR CURRENT LIMIT
+float READ_CPILOT_CURRENT() {
+  return 0l
 }
 
 // CAN STUFF BEGIN
@@ -484,11 +488,8 @@ int main(void)
   //   printBmsAndElconData(&currentBmsAndElconData);
   //   HAL_Delay(1000);
   // }
-  FAN_SPD_CTRL(100); // TODO: make this based on temp
-  uint16_t therm_inlet_value = adc_buffer[0];
-  uint16_t therm_outlet_value = adc_buffer[1];
-  therm_inlet = &therm_inlet_value;
-  therm_outlet = &therm_outlet_value;
+  therm_inlet = &adc_buffer[0];
+  therm_outlet = &adc_buffer[1];
   GPIO_PinState IN_HVIL_SW_STATE;
   GPIO_PinState RTC_SW_STATE;
   GPIO_PinState IN_HVIL_ESTOP_Pin_State;
@@ -506,6 +507,10 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+
+    int fan_speed = READ_THERM(therm_outlet, THERM_RESIST);
+    FAN_SPD_CTRL(fan_speed);
+
     // TODO: CHECK ALL LEDS AND PERIPHERALS WORK
     //TODO: DOUBLE CHECK
     if (!isCharging && !isBalancing && !isChargingSequence) {
